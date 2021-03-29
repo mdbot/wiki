46ba7bcc02146f3bc747d734d93c24c9948b54b0
Greg Holmes <git@greg.holmes.name>
2021-03-29 21:28:26 +0100

    Add Put Page method

--- wiki.go
+++ wiki.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"os"
+	"time"
 )
 
 type GitPageProvider struct {
@@ -47,4 +49,31 @@ func (g *GitPageProvider) GetPage(path string) (*Page, error) {
 			Message:  commit.Message,
 		},
 	}, nil
+}
+
+func (g *GitPageProvider) PutPage(title string, content []byte, user string, message string) error {
+	title = title + ".md"
+	err := os.WriteFile(g.GitDirectory + "/" + title, content, os.FileMode(0644))
+	if err != nil {
+		return err
+	}
+	worktree, err := g.GitRepo.Worktree()
+	if err != nil {
+		return err
+	}
+	_, err = worktree.Add(title)
+	if err != nil {
+		return err
+	}
+	_, err = worktree.Commit(message, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  user,
+			Email: user+"@wiki",
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
