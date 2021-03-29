489e91c740cba3bbd5e6237df0a248cea3b6bfaf
Chris Smith <chris@chameth.com>
2021-03-29 23:07:36 +0100

    Actually make sure we don't use bad paths.
    This is more complicated when it has to work, rather than
    just pass tests...

--- wiki_test.go
+++ wiki_test.go
@@ -11,31 +11,90 @@ func Test_resolvePath(t *testing.T) {
 		title string
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
+		name     string
+		args     args
+		wantFile string
+		wantGit  string
+		wantErr  bool
 	}{
-		{"basic file", args{"repo", "title"}, filepath.Join("repo", "title.md"), false},
-		{"sub directory", args{"repo", "some/folder/title"}, filepath.Join("repo", "some", "folder", "title.md"), false},
-		{"collapse dirs", args{"repo", "some/folder/../../title"}, filepath.Join("repo", "title.md"), false},
-		{"normalise dots", args{"repo", "./title"}, filepath.Join("repo", "title.md"), false},
-		{"directory escape", args{"repo", "../title"}, "", true},
-		{"directory escape", args{"repo", "foo/../../title"}, "", true},
-		{"relative git directory", args{"repo", "./.git/refs"}, "", true},
-		{"git directory", args{"repo", ".git/refs"}, "", true},
-		{"git subdirectory", args{"repo", "foo/bar/.git/refs"}, "", true},
-		{"mixed-case git", args{"repo", "foo/bar/.GiT/refs"}, "", true},
+		{
+			"basic file",
+			args{"repo", "title"},
+			filepath.Join("repo", "title.md"),
+			"title.md",
+			false,
+		},
+		{
+			"sub directory",
+			args{"repo", "some/folder/title"},
+			filepath.Join("repo", "some", "folder", "title.md"),
+			filepath.Join("some", "folder", "title.md"),
+			false,
+		},
+		{
+			"collapse dirs",
+			args{"repo", "some/folder/../../title"},
+			filepath.Join("repo", "title.md"),
+			filepath.Join("title.md"),
+			false,
+		},
+		{
+			"normalise dots",
+			args{"repo", "./title"},
+			filepath.Join("repo", "title.md"),
+			filepath.Join("title.md"),
+			false,
+		},
+		{
+			"directory escape",
+			args{"repo", "../title"},
+			"",
+			"",
+			true,
+		},
+		{
+			"nested directory escape",
+			args{"repo", "foo/../../title"},
+			"",
+			"",
+			true,
+		},
+		{
+			"relative git directory",
+			args{"repo", "./.git/title"},
+			"",
+			"",
+			true,
+		},
+		{
+			"git subdirectory",
+			args{"repo", "foo/bar/.git/title"},
+			"",
+			"",
+			true,
+		},
+		{
+			"git mixed-case",
+			args{"repo", "foo/bar/.gIt/title"},
+			"",
+			"",
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := resolvePath(tt.args.base, tt.args.title)
+			gotFilePath, gotGitPath, err := resolvePath(tt.args.base, tt.args.title)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("resolvePath() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
-				t.Errorf("resolvePath() got = %v, want %v", got, tt.want)
+			if gotFilePath != tt.wantFile {
+				t.Errorf("resolvePath() got file path = %v, want %v", gotFilePath, tt.wantFile)
+				return
+			}
+			if gotGitPath != tt.wantGit {
+				t.Errorf("resolvePath() got git path = %v, want %v", gotGitPath, tt.wantGit)
+				return
 			}
 		})
 	}
