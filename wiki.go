47ead77a4a63ce4b273b6348046c27d2c20a69c5
Greg Holmes <git@greg.holmes.name>
2021-03-29 21:03:26 +0100

    Add rough getPage function

--- wiki.go
+++ wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-git/go-git/v5"
+	"os"
 )
 
 func openOrInit(dataDirectory string) (*git.Repository, error) {
@@ -14,4 +15,31 @@ func openOrInit(dataDirectory string) (*git.Repository, error) {
 		return gitRepo, nil
 	}
 	return nil, err
+}
+
+func getPage(repo *git.Repository, path string) (*Page, error) {
+	commitIter, err := repo.Log(&git.LogOptions{
+		FileName: &path,
+	})
+	if err != nil {
+		return nil, err
+	}
+	commit, err := commitIter.Next()
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := os.ReadFile(*workDir+"/"+path)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{
+		Title:        path,
+		Content:      string(bytes),
+		LastModified: &LogEntry{
+			ChangeId: commit.Hash.String(),
+			User:     commit.Author.Name,
+			Time:     commit.Author.When,
+			Message:  commit.Message,
+		},
+	}, nil
 }
