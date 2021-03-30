c468152d2aa86483ddc364bdf003f462067cf69b
Greg Holmes <git@greg.holmes.name>
2021-03-30 22:20:14 +0100

    Tidy go imports in git.go

--- git.go
+++ git.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-git/go-git/v5/plumbing"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,6 +10,7 @@ import (
 	"time"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
