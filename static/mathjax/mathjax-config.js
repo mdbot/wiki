b9ed2181d175280ee90df2da098165c8d2ccb3eb
Greg Holmes <git@greg.holmes.name>
2021-04-01 23:53:46 +0100

    Hide mathjax until the script to display is loaded.
    Fixes #59

--- static/mathjax/mathjax-config.js
+++ static/mathjax/mathjax-config.js
@@ -9,6 +9,11 @@
         }
         let script = document.createElement('script');
         script.src = '/mathjax/mathjax-3.1.2.js';
+        script.onload = function() {
+        [].forEach.call(document.querySelectorAll('.math'), function (el) {
+                el.classList.remove('math');
+            });
+        }
         document.head.appendChild(script);
     }
 })();
