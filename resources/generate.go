22cc16efb8ed3951b3ff056701a230d28a33cc6b
Chris Smith <chris@chameth.com>
2021-04-07 17:06:19 +0100

    Rough and ready JS generation

--- resources/generate.go
+++ resources/generate.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"archive/zip"
+	"bytes"
+	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/evanw/esbuild/pkg/api"
 )
@@ -20,10 +24,49 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	// Extract zip to dir
-	// Copy codemirror.js to dir
-	// Run builder with the dir as the base oath or whatever
-	_ = dir
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	zr, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range zr.File {
+		func(f *zip.File) {
+			if f.FileInfo().IsDir() {
+				return
+			}
+
+			file, err := f.Open()
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+
+			bs, err := io.ReadAll(file)
+			if err != nil {
+				panic(err)
+			}
+
+			target := strings.ReplaceAll(filepath.Join(dir, f.Name), "codemirror-5.60.0", "codemirror")
+
+			if err := os.MkdirAll(filepath.Dir(target), os.FileMode(0755)); err != nil {
+				panic(err)
+			}
+
+			if err := os.WriteFile(target, bs, os.FileMode(0644)); err != nil {
+				panic(err)
+			}
+		}(zr.File[i])
+	}
+
+	copy("resources/editor.js", dir)
+	copy("resources/editor.css", dir)
+
+	outFile, _ := filepath.Abs("./resources/static/")
 
 	res := api.Build(api.BuildOptions{
 		Bundle:            true,
@@ -31,9 +74,24 @@ func main() {
 		MinifySyntax:      true,
 		MinifyWhitespace:  true,
 		Write:             true,
-		EntryPoints:       []string{"resources/codemirror.js"},
-		Outfile:           "resources/static/codemirror-min.js",
+		AbsWorkingDir:     dir,
+		EntryPoints:       []string{"editor.js", "editor.css"},
+		Outdir:            outFile,
 	})
 
-	fmt.Printf("%v\n", res)
+	if len(res.Errors) > 0 {
+		panic(res.Errors[0].Text)
+	}
+
+	os.RemoveAll(dir)
+}
+
+func copy(file, dir string) {
+	bs, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	if err := os.WriteFile(filepath.Join(dir, filepath.Base(file)), bs, os.FileMode(0644)); err != nil {
+		panic(err)
+	}
 }
