20b80122f21cc74e3fcdec2cd1f9dc95dbd5e0fa
Chris Smith <chris@chameth.com>
2021-04-07 17:21:00 +0100

    Tidy up generator a little

--- resources/generate.go
+++ resources/generate.go
@@ -18,7 +18,33 @@ func main() {
 		panic(err)
 	}
 
-	resp, err := http.Get("https://codemirror.net/codemirror-5.60.0.zip")
+	codemirrorBytes := downloadFile("https://codemirror.net/codemirror-5.60.0.zip")
+	extractZip(codemirrorBytes, dir, "codemirror-5.60.0", "codemirror")
+	copyFile("resources/editor.js", dir)
+	copyFile("resources/editor.css", dir)
+
+	outFile, _ := filepath.Abs("./resources/static/")
+
+	res := api.Build(api.BuildOptions{
+		Bundle:            true,
+		MinifyIdentifiers: true,
+		MinifySyntax:      true,
+		MinifyWhitespace:  true,
+		Write:             true,
+		AbsWorkingDir:     dir,
+		EntryPoints:       []string{"editor.js", "editor.css"},
+		Outdir:            outFile,
+	})
+
+	if len(res.Errors) > 0 {
+		panic(res.Errors[0].Text)
+	}
+
+	os.RemoveAll(dir)
+}
+
+func downloadFile(url string) []byte {
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
@@ -28,6 +54,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	return b
+}
+
+func copyFile(file, dir string) {
+	bs, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	if err := os.WriteFile(filepath.Join(dir, filepath.Base(file)), bs, os.FileMode(0644)); err != nil {
+		panic(err)
+	}
+}
+
+func extractZip(b []byte, dir string, replacements... string) {
+	replacer := strings.NewReplacer(replacements...)
 
 	zr, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
 	if err != nil {
@@ -51,7 +93,7 @@ func main() {
 				panic(err)
 			}
 
-			target := strings.ReplaceAll(filepath.Join(dir, f.Name), "codemirror-5.60.0", "codemirror")
+			target := replacer.Replace(filepath.Join(dir, f.Name))
 
 			if err := os.MkdirAll(filepath.Dir(target), os.FileMode(0755)); err != nil {
 				panic(err)
@@ -61,37 +103,5 @@ func main() {
 				panic(err)
 			}
 		}(zr.File[i])
-	}
-
-	copy("resources/editor.js", dir)
-	copy("resources/editor.css", dir)
-
-	outFile, _ := filepath.Abs("./resources/static/")
-
-	res := api.Build(api.BuildOptions{
-		Bundle:            true,
-		MinifyIdentifiers: true,
-		MinifySyntax:      true,
-		MinifyWhitespace:  true,
-		Write:             true,
-		AbsWorkingDir:     dir,
-		EntryPoints:       []string{"editor.js", "editor.css"},
-		Outdir:            outFile,
-	})
-
-	if len(res.Errors) > 0 {
-		panic(res.Errors[0].Text)
-	}
-
-	os.RemoveAll(dir)
-}
-
-func copy(file, dir string) {
-	bs, err := os.ReadFile(file)
-	if err != nil {
-		panic(err)
-	}
-	if err := os.WriteFile(filepath.Join(dir, filepath.Base(file)), bs, os.FileMode(0644)); err != nil {
-		panic(err)
 	}
 }
